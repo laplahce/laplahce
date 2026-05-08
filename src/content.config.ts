229912fddea23a139b152b47f310ce2b60f73c39
LeoPilhamre <laplahce@gmail.com>
2026-05-08 21:29:42 +0200

    Further setup

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,6 @@ import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 const packages = defineCollection({
-  // Ensure this path matches your folder structure in the repo
   loader: glob({ pattern: "**/*.md", base: "./src/content/packages" }),
   schema: z.object({
     title: z.string(),

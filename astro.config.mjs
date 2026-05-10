8c5384b73c4d2beefab53ebc9436be8dfd3a368a
LeoPilhamre <laplahce@gmail.com>
2026-05-10 11:18:14 +0200

    Deleted tailwind x3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   // Ensures your site builds as a fast, static website
   output: 'static',
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
